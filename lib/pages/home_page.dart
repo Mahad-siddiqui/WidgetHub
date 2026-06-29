4a87ffe56e237d0eda96617a4b03e99592d20bea
Mahad-siddiqui <m.mahadsiddiquii@gmail.com>
2026-06-30 00:39:49 +0500

    12 Refactor HomePage to display products in a GridView with enhanced item layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,10 +45,55 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(title: const Text("Widget Hub")),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
+          // : ListView.builder(
+          //     itemCount: CatalogModel.products.length,
+          //     itemBuilder: (context, index) {
+          //       return ItemWidget(item: CatalogModel.products[index]);
+          //     },
+          //   ),
+          : GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16.0,
+                crossAxisSpacing: 16.0,
+              ),
               itemCount: CatalogModel.products.length,
               itemBuilder: (context, index) {
-                return ItemWidget(item: CatalogModel.products[index]);
+                final item = CatalogModel.products[index];
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: GridTile(
+                    child: Image.network(item.image, fit: BoxFit.cover),
+                    header: Container(
+                      color: const Color.fromARGB(136, 100, 65, 97),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        item.name,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    footer: Container(
+                      color: const Color.fromARGB(137, 48, 22, 58),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        item.price.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                );
               },
             ),
     );
